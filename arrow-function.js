19c6ee66f2a634246b22edd379b20d93a87d3491
chaitey50 <chaiteychaitey30@gmail.com>
2026-02-02 23:54:29 +0600

    arrow-funtion

---
+++ arrow-function.js
@@ -0,0 +1,5 @@
+function add(num1, num2) {
+    return num1 + num2
+}
+const add2 = (num1, num2) => num1 * num2
+console.log(add2(10, 10));
---
+++ default-parameter.js
@@ -0,0 +1,10 @@
+function add(num1, num2 = 0) {
+    const result = num1 + num2;
+    // console.log(num1, num2, result);
+}
+add(10);
+function multipy(num1, num2 = 1) {
+    const result = num1 * num2;
+    console.log(result);
+}
+multipy(3)
