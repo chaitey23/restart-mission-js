e5838f23a5585cda7858479343e476d42b2a9133
chaitey50 <chaiteychaitey30@gmail.com>
2026-02-03 16:05:33 +0600

    recap spread operator

--- arrow-function.js
+++ arrow-function.js
@@ -3,3 +3,20 @@ function add(num1, num2) {
 }
 const add2 = (num1, num2) => num1 * num2
 console.log(add2(10, 10));
+
+// single arrow function
+const getSquare = (x) => x * x;
+const getHalf = num => num / 2;
+const firstElement = nums => nums[0];
+console.log(getSquare(4));
+console.log(getHalf(10));
+console.log(firstElement([12, 99, 78]));
+
+// multiline arrow function
+const doMath = (x, y) => {
+    const makeDouble = x * 2;
+    const againDouble = y * 2;
+    const result = makeDouble + againDouble;
+    return result
+}
+console.log(doMath(2, 4),);
--- spread.js
+++ spread.js
@@ -6,3 +6,22 @@
 
 // const number = [41,55,78,3652,8852];
 // console.log(Math.max(...number));
+const number = [23, 45, 111, 90]
+console.log(number);
+console.log(...number);
+const max = Math.max(...number)
+console.log(max);
+const first = [1, 2, 3, 4, 5];
+const second = [...first];
+const third = [...first, 9, 8, 7]
+second.push(6);
+console.log(second);
+console.log(third);
+const person = {
+    name: "chaitey",
+    age: 20
+}
+const employee = {
+    designation: "dev", ...person
+};
+console.log(employee);
