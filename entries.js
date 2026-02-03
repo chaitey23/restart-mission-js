374d6572cc1bdf68cecdc880f3bbc9bc853fea92
chaitey50 <chaiteychaitey30@gmail.com>
2026-02-03 16:41:34 +0600

    entries,freeze,seal

---
+++ entries.js
@@ -0,0 +1,12 @@
+const employee = {
+    name: "chaitey",
+    designation: "developer",
+    salary: "15000",
+    experience: 1
+}
+const keys = Object.keys(employee);
+console.log(keys);
+const value = Object.values(employee)
+console.log(value);
+const entries = Object.entries(employee);
+console.log(entries);
---
+++ freeze.js
@@ -0,0 +1,10 @@
+const employee = {
+    name: "chaitey",
+    designation: "developer",
+    salary: 15000,
+    experience: 1
+}
+Object.freeze(employee)
+employee.salary = employee.salary + 5000
+// delete employee.experience
+console.log(employee);
