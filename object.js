808a21ccbcfa198059ddabfd12da45a94a4292fd
chaitey50 <chaiteychaitey30@gmail.com>
2026-02-05 23:54:38 +0600

    object

--- object.js
+++ object.js
@@ -1,35 +1,35 @@
-// const persone ={
-//     name : 'chaitey',
-//     age : 20,
-//     country : 'BD'
-// }
+const persone = {
+  name: 'chaitey',
+  age: 20,
+  country: 'BD'
+}
 
-// console.log(Object.entries(persone));
+console.log(Object.entries(persone));
 
-// for(let [key,value] of Object.entries(persone)){
-//   console.log(`key : ${key}  value : ${value}`);
-  
-    
-// }
+for (let [key, value] of Object.entries(persone)) {
+  console.log(`key : ${key}  value : ${value}`);
 
 
-
-
-// Object.seal(persone)
-// Object.freeze(persone)
-// persone.status = 'not found'
-// persone.country = 'USA'
-// console.log(persone);
-
-
-// console.log(Object.keys(persone));
-// console.log(Object.values(persone));
-// console.log(Object.entries(persone));
+}
 
 
 
-const user  = {
-  name : 'chaitey'
+
+Object.seal(persone)
+Object.freeze(persone)
+persone.status = 'not found'
+persone.country = 'USA'
+console.log(persone);
+
+
+console.log(Object.keys(persone));
+console.log(Object.values(persone));
+console.log(Object.entries(persone));
+
+
+
+const user = {
+  name: 'chaitey'
 }
 const zip = user?.address?.zip
 
