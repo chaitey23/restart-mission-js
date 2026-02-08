6815935b6b14a4a072071876d20e5279aeb9f18b
chaitey50 <chaiteychaitey30@gmail.com>
2026-02-08 22:53:24 +0600

    destructure.js

--- destructure.js
+++ destructure.js
@@ -1,19 +1,19 @@
-// const hane = {
-//     name : 'hena',
-//     age : 19,
-//     friends : ['hero alam','bapparaj'],
-//     status : 'not found'
-// }
-// const { name,age,status,friends} = hane;
-// console.log(status);
+const hane = {
+    name: 'hena',
+    age: 19,
+    friends: ['hero alam', 'bapparaj'],
+    status: 'not found'
+}
+const { name, age, status, friends } = hane;
+console.log(status);
 
 
 
-// const person = ['hero alam','bapparaj','hena']
+const person = ['hero alam', 'bapparaj', 'hena']
 
 
-// const [hero,sadNayok,naika] = person
-// console.log(naika);
+const [hero, sadNayok, naika] = person
+console.log(naika);
 
 const { price } = { name: "phone", brand: "realme", price: 20000 }
 // const { brand } = device;
