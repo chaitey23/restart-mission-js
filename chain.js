9b6b1a0cc3dafd5bd10453de2fff06435210354f
chaitey50 <chaiteychaitey30@gmail.com>
2026-02-09 17:28:44 +0600

    chaining

---
+++ chain.js
@@ -0,0 +1,14 @@
+const student = {
+    name: "chaitey",
+    1: 50,
+    family: {
+        title: "chowdhury",
+        mother: {
+            name: "chaina begum",
+            age: 38
+        }
+    },
+    marks: 90,
+    "home-town": "Rangpur"
+}
+console.log(student.family.mother?.age);
