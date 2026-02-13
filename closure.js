b8e23867ce26ad43bbd7eeb761389e9b2c5ea984
chaitey50 <chaiteychaitey30@gmail.com>
2026-02-13 17:26:36 +0600

    learn closure funtion

--- closure.js
+++ closure.js
@@ -1,10 +1,44 @@
-// function sum(){
-//     let counter = 0;
-//   return function () {
-//         counter++;
-//         console.log(counter);
-//     }
-// }
-// const result = sum()
-// result()
-// result()
+function sum() {
+    let counter = 0;
+    return function () {
+        counter++;
+        console.log(counter);
+    }
+}
+const result = sum()
+result()
+result()
+
+
+function counter() {
+    let count = 0;
+    function innerFunction() {
+        console.log("Inner the inner function,some one called me");
+    }
+    return innerFunction;
+}
+const output = counter();
+console.log(output());
+function counter() {
+    let count = 0;
+    return function () {
+        count = count + 1;
+        console.log("Inner the inner function", count);
+
+    }
+}
+const innerFucn = counter();
+innerFucn();
+innerFucn()
+innerFucn()
+innerFucn()
+innerFucn()
+innerFucn()
+innerFucn()
+const rahimCountet = counter();
+rahimCountet();
+rahimCountet();
+rahimCountet();
+rahimCountet();
+rahimCountet();
+innerFucn()
